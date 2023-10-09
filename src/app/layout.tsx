de066d7c41bd66406ad4d5860089bef89bb82a0c
Kessy Similien <simkessy@gmail.com>
2023-10-09 12:55:57 -1000

    start google auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { signIn } from 'next-auth/react';
 
-
+import GoogleButton from 'react-google-button';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>{children}</body>
+      <GoogleButton className='mx-auto mt-16'/>
       <Analytics />
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
-import 'dotenv/config'
-import getSheetsData from './api/sheets'
-
+import "dotenv/config";
+import getSheetsData from "./api/sheets";
 
 export default async function Home() {
-  const sheetdata = await getSheetsData()
+  const sheetdata = await getSheetsData();
 
-  console.log('sheetdata', sheetdata)
+  console.log("sheetdata", sheetdata);
 
-  const user = JSON.parse(process.env.GOOGLE_MADAM_HUNGRY ?? "{}")?.project_id || "default user"
+  const user =
+    JSON.parse(process.env.GOOGLE_MADAM_HUNGRY ?? "{}")?.project_id ||
+    "default user";
 
   return (
     <>
       <h1>Hello {user}</h1>
       <h3>Sheet Data</h3>
-      <ul>{
-        sheetdata.map((data,index)=> <li key={index}>{data}-{index}</li>)
-      }</ul>
+      <ul>
+        {sheetdata.map((data, index) => (
+          <li key={index}>
+            {data}-{index}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
