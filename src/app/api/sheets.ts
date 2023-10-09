514635d010bd6467329de45ef41157e4c56fe8aa
Kessy Similien <simkessy@gmail.com>
2023-10-08 20:54:01 -1000

    Get data from sheet

--- src/app/api/sheets.ts
+++ src/app/api/sheets.ts
@@ -1,53 +1,38 @@
 import { google } from "googleapis";
-import { NextApiRequest, NextApiResponse } from 'next';
 import 'dotenv/config'
 
 const CREDENTIALS = JSON.parse(process.env.GOOGLE_MADAM_HUNGRY ?? "{}")
 
-interface MyResponse {
-    error: boolean;
-    message?: string;
-    data?: any;
-}
+type SheetResponse = Array<string>
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<MyResponse>
-) {
-    try {
-        const client = new google.auth.JWT(
-            CREDENTIALS.client_email,
-            undefined,
-            CREDENTIALS.private_key,
-            ['https://www.googleapis.com/auth/spreadsheets']
-        );
-
-        client.authorize(async function (err, tokens) {
-            if (err) {
-                res.status(400).json({ error: true });;
-                return;
-            }
+export default function getSheetsData():Promise<Array<SheetResponse>> {
+    return new Promise((resolve, reject) => {
+        try {
+            const client = new google.auth.JWT(
+                CREDENTIALS.client_email,
+                undefined,
+                CREDENTIALS.private_key,
+                ['https://www.googleapis.com/auth/spreadsheets']
+            );
 
             const gsapi = google.sheets({ version: 'v4', auth: client });
 
-            //CUSTOMIZATION FROM HERE
             const opt = {
                 spreadsheetId: process.env.GOOGLE_SHEETS_ID,
-                range: 'Sheet1!A2:A'
+                range: 'FormResponses!A2:A'
             };
 
-            try {
-                const data = await gsapi.spreadsheets.values.get(opt);
-                res.status(200).json({ error: false, data: data.data.values });
-            } catch (error) {
-                // Handle error of type unknown
-                const errorMessage = (error as Error).message || 'Unknown error';
-                res.status(400).json({ error: true, message: errorMessage });
-            }
-        });
-    } catch (error) {
-        // Handle error of type unknown
-        const errorMessage = (error as Error).message || 'Unknown error';
-        res.status(400).json({ error: true, message: errorMessage });
-    }
+            gsapi.spreadsheets.values.get(opt, (err:any, res:any) => {
+                if (err) {
+                    const errorMessage = err.message || 'Unknown error';
+                    reject(Error(errorMessage));
+                } else {
+                    resolve(res?.data.values);
+                }
+            });
+        } catch (error) {
+            const errorMessage = (error as Error).message || 'Unknown error';
+            reject(Error(errorMessage));
+        }
+    });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import 'dotenv/config'
+import getSheetsData from './api/sheets'
 
-export default function Home() {
-  console.log(process.env)
+
+export default async function Home() {
+  const sheetdata = await getSheetsData()
+
+  console.log('sheetdata', sheetdata)
+
   const user = JSON.parse(process.env.GOOGLE_MADAM_HUNGRY ?? "{}")?.project_id || "default user"
 
   return (
+    <>
       <h1>Hello {user}</h1>
+      <h3>Sheet Data</h3>
+      <ul>{
+        sheetdata.map((data,index)=> <li key={index}>{data}-{index}</li>)
+      }</ul>
+    </>
   )
 }
