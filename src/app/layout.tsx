3d31605bfaa5045900169380769f58c7781af2b0
Kessy Similien <simkessy@gmail.com>
2023-10-08 15:45:15 -1000

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.classNamev} bg-slate-800 text-slate-100 container mx-auto p-4`}>{children}</body>
     </html>
   )
 }
+ 
