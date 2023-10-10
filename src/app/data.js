68dea186bb56ab3eba36921ac98ee967061a564a
Kessy Similien <simkessy@gmail.com>
2023-10-10 13:25:56 -1000

    Create test data

---
+++ src/app/data.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    id: 1,
+    assignedTo: "Amanda",
+    title:
+      "A Global Analysis of Climate Change and the Impacts on Oyster Diseases",
+    authors:
+      "Okon, Ekemini Moses; Birikorang, Harriet Nketiah; Munir, Mohammad Bodrul; Kari, Zulhisyam Abdul; Téllez-Isaías, Guillermo; Khalifa, Norhan E.; Abdelnour, Sameh A.; Eissa, Moaheda E. H.; Al-Farga, Ammar; Dighiesh, Hagar Sedeek; Eissa, El-Sayed Hemdan",
+    year: 2023,
+    journal: "Sustainability",
+    pages: 12775,
+    doi: "10.3390/su151712775",
+    url: "https://www.mdpi.com/2071-1050/15/17/12775",
+  },
+  {
+    id: 2,
+    assignedTo: "Amanda",
+    title:
+      "An updated review on behavior of domestic quail with reference to the negative effect of heat stress",
+    authors:
+      "Batool, Fiza; Bilal, Rana M.; Hassan, Faiz Ul; Nasir, Taquir A.; Rafeeque, Majid; Elnesr, Shaaban S.; Farag, Mayada R.; Mahgoub, Hany A. M.; Naiel, Mohammed A. E.; Alagawany, Mahmoud",
+    year: 2023,
+    journal: "Animal Biotechnology",
+    pages: "424-437",
+    doi: "10.1080/10495398.2021.1951281",
+    url: "https://doi.org/10.1080/10495398.2021.1951281",
+  },
+  {
+    id: 3,
+    assignedTo: "Anna",
+    title:
+      "Changes in the Potential Distribution of Vanilla planifolia Andrews under Different Climate Change Projections in Mexico",
+    authors:
+      "Armenta-Montero, Samaria; Menchaca-García, Rebeca; Pérez-Silva, Araceli; Velázquez-Rosas, Noé",
+    year: 2022,
+    journal: "Sustainability",
+    pages: 2881,
+    doi: "10.3390/su14052881",
+    url: "https://www.mdpi.com/2071-1050/14/5/2881",
+  },
+];
+
+export default data;
