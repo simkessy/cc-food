915a88c783472431efd15d09670b6514ffe71147
Kessy Similien <simkessy@gmail.com>
2023-10-10 09:50:16 -1000

    clean up

--- src/app/components/UserName.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-
-export const UserName = () => {
-  const { data } = useSession();
-  const user = data?.user?.name || "default user";
-  return <h1>Hello, {user}</h1>;
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import getSheetsData from "./api/sheets";
 import { SheetData } from "./components/SheetData";
-import { UserName } from "./components/UserName";
+import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const sheetdata = await getSheetsData();
+  const session = await getServerSession();
+  const user = session?.user?.name;
 
   return (
     <>
-      <UserName />
+      {user && <h1>Hello {user}</h1>}
       <SheetData sheetdata={sheetdata} />
     </>
   );
