e38cd44eaf25e837debd8d4e1f120a60499ea106
Kessy Similien <simkessy@gmail.com>
2023-10-09 21:10:36 -1000

    Add gestalt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import 'dotenv/config'
-import { Analytics } from '@vercel/analytics/react';
+import "dotenv/config";
+import { Analytics } from "@vercel/analytics/react";
 
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import "gestalt/dist/gestalt.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-
-
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'cc-food',
-  description: 'WeHungry Form',
-}
+  title: "cc-food",
+  description: "WeHungry Form",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}
+      >
+        {children}
+      </body>
       <Analytics />
     </html>
-  )
+  );
 }
