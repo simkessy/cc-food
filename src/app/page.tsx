54fdb73bce255df391ad007a521c0687256ce231
Kessy Similien <simkessy@gmail.com>
2023-10-11 14:35:52 -1000

    Reduce components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import {
-  Box,
-  Flex,
-} from "gestalt";
+import { Box, Flex } from "gestalt";
 import { useSession } from "next-auth/react";
 import { AppBar } from "./components/AppBar";
 import Form from "./components/Form";
@@ -12,20 +9,18 @@ export default function Home() {
   const user = session.data?.user;
 
   return (
-    <Box color="default" height="100vh" width="100vw">
-      <Flex height="100%">
-        <Box column={12} padding={6}>
-          {/* Nav bar  */}
-          <Box color="education" borderStyle="lg" fit>
-            {/* Auth button */}
-            <AppBar />
-          </Box>
-          {/* Form body */}
-          {user && (
-            <Form />
-          )}
+    <Box color="recommendationBase" height="100vh" width="100vw" padding={2}>
+      {/* Nav bar  */}
+      <Box color="education" borderStyle="lg" fit>
+        {/* Auth button */}
+        <AppBar />
+      </Box>
+      {/* Form body */}
+      {user && (
+        <Box color="infoBase" borderStyle="lg" fit>
+          <Form />
         </Box>
-      </Flex>
+      )}
     </Box>
   );
 }
