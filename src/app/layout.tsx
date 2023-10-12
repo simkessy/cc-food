f627121925de83272365b8c7057bce1d77fb4a86
Kessy Similien <simkessy@gmail.com>
2023-10-11 14:17:06 -1000

    clean up layout imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,8 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import "gestalt/dist/gestalt.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import SessionProvider from "./context/SessionProvider";
-import { AppBar } from "./components/AppBar";
 import { getServerSession } from "next-auth";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "cc-food",
